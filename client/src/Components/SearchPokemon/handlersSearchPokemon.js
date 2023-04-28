f9015654edf48695ebe8fe9c126c6f093221c27e
martin marchessi <niitram.mm@gmail.com>
2023-04-28 09:43:49 -0300

    arreglado de search

--- client/src/Components/SearchPokemon/handlersSearchPokemon.js
+++ client/src/Components/SearchPokemon/handlersSearchPokemon.js
@@ -7,11 +7,28 @@ export const handlerSubmit = async (e, nameSearch, setSearching, setPokemonFound
         if (nameSearch.length > 0) {
             setSearching(true)
             const response = await axios.get(`http://localhost:3001/pokemons/name`, { params: { name: nameSearch } })
+            let pokemonData = {}
+            if (Array.isArray(response.data)) {
+                pokemonData = {
+                    name: response.data[0].name,
+                    attack: response.data[0].attack,
+                    hp: response.data[0].hp,
+                    defense: response.data[0].defense,
+                    height: response.data[0].height,
+                    speed: response.data[0].speed,
+                    weight: response.data[0].weight,
+                    id: response.data[0].id,
+                    image: response.data[0].image,
+                    types: response.data[0].types.map(tipo => tipo.name)
+                }
+            } else {
+                pokemonData = { ...response.data }
+            }
             if (response.data === "pokemon no encontrado") {
                 setPokemonFound(false)
             } else {
                 setPokemonFound(true)
-                dispatch(searchByName(response.data))
+                dispatch(searchByName(pokemonData))
             }
         }
     } catch (error) {
